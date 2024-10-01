f30052fb553a0b7bf702d61ed45cdb9772be1d5e
level237 <Bramslevel129@gmail.com>
2024-10-01 12:04:43 +0100

    update

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -4,8 +4,15 @@ import NavBar from '../components/NavBar'
 import { Separator } from '../components/ui/separator'
 import ListBoard from '../components/ListBoard'
 import { Button } from '../components/ui/button'
-import { Filter, Plus } from 'lucide-react'
-
+import { DotIcon, Filter, MoreHorizontal, Plus } from 'lucide-react'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "../components/ui/card"
 export default function DashboardPage() {
   return (
     <>
@@ -33,35 +40,149 @@ export default function DashboardPage() {
          </div>
     
     
-      <div className="grid grid-cols-3 gap-4">
-         <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-            <p className="text-2xl text-gray-400 dark:text-gray-500">
-               <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-               </svg>
-            </p>
-         </div>
-         <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-            <p className="text-2xl text-gray-400 dark:text-gray-500">
-               <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-               </svg>
-            </p>
-         </div>
-         <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-            <p className="text-2xl text-gray-400 dark:text-gray-500">
-               <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-               </svg>
-            </p>
-         </div>
-         <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
-            <p className="text-2xl text-gray-400 dark:text-gray-500">
-               <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                  <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16"/>
-               </svg>
-            </p>
-         </div>
+      <div className="grid grid-cols-4 gap-4">
+
+<div>
+<Card className="">
+      <CardHeader>
+        <CardTitle>Todo List</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent className='flex flex-col gap-3'>
+      <div className='w-full flex flex-col  rounded-md px-3 py-4 bg-[#1271ff41]'>
+        <div className='flex mb-3 items-center justify-between'>
+            <div className='px-2 rounded-lg  bg-[#1271ff]'>
+                <h2 className='text-xs text-white'>Urgent</h2>
+                
+            </div>
+            <div>
+            <MoreHorizontal className='w-4 h-4 text-[#1271ff]'/>
+            </div>
+        </div>
+    <h2 className='text-lg text-[#1271ff] font-bold'>Move down</h2>
+    <div>
+        <span className='text-[#1271ff] line-clamp-3 mt-2 text-xs'>Manage men sevent game,let me down,let me see you please</span>
+        </div>
+        <div className='flex justify-end mt-3'>
+        <div className='px-2 rounded-lg border border-[#1271ff]  bg-transparent'>
+                <h2 className='text-xs text-[#1271ff]'>12 oct 2024</h2>
+                
+            </div>
+        </div>
+        </div>
+        <div className='w-full flex flex-col  rounded-md px-3 py-4 bg-[#1271ff41]'>
+        <div className='flex mb-3 items-center justify-between'>
+            <div className='px-2 rounded-lg  bg-[#1271ff]'>
+                <h2 className='text-xs text-white'>Urgent</h2>
+                
+            </div>
+            <div>
+            <MoreHorizontal className='w-4 h-4 text-[#1271ff]'/>
+            </div>
+        </div>
+    <h2 className='text-lg text-[#1271ff] font-bold'>Move down</h2>
+    <div>
+        <span className='text-[#1271ff] line-clamp-3 mt-2 text-xs'>Manage men sevent game,let me down,let me see you please</span>
+        </div>
+        <div className='flex justify-end mt-3'>
+        <div className='px-2 rounded-lg border border-[#1271ff]  bg-transparent'>
+                <h2 className='text-xs text-[#1271ff]'>12 oct 2024</h2>
+                
+            </div>
+        </div>
+        </div>
+
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        
+      </CardFooter>
+    </Card>
+</div>
+<div>
+<Card className="">
+      <CardHeader>
+        <CardTitle>In Progress</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent className='flex flex-col gap-3'>
+      <div className='w-full flex flex-col  rounded-md px-3 py-4 bg-[#d7790660]'>
+        <div className='flex mb-3 items-center justify-between'>
+            <div className='px-2 rounded-lg  bg-[#d77906]'>
+                <h2 className='text-xs text-white'>Urgent</h2>
+                
+            </div>
+            <div>
+            <MoreHorizontal className='w-4 h-4 text-[#d77906]'/>
+            </div>
+        </div>
+    <h2 className='text-lg text-[#d77906] font-bold'>Move down</h2>
+    <div>
+        <span className='text-[#d77906] line-clamp-3 mt-2 text-xs'>Manage men sevent game,let me down,let me see you please</span>
+        </div>
+        <div className='flex justify-end mt-3'>
+        <div className='px-2 rounded-lg border border-[#d77906]  bg-transparent'>
+                <h2 className='text-xs text-[#d77906]'>12 oct 2024</h2>
+                
+            </div>
+        </div>
+        </div>
+        <div className='w-full flex flex-col  rounded-md px-3 py-4 bg-[#1271ff41]'>
+        <div className='flex mb-3 items-center justify-between'>
+            <div className='px-2 rounded-lg  bg-[#1271ff]'>
+                <h2 className='text-xs text-white'>Urgent</h2>
+                
+            </div>
+            <div>
+            <MoreHorizontal className='w-4 h-4 text-[#1271ff]'/>
+            </div>
+        </div>
+    <h2 className='text-lg text-[#1271ff] font-bold'>Move down</h2>
+    <div>
+        <span className='text-[#1271ff] line-clamp-3 mt-2 text-xs'>Manage men sevent game,let me down,let me see you please</span>
+        </div>
+        <div className='flex justify-end mt-3'>
+        <div className='px-2 rounded-lg border border-[#1271ff]  bg-transparent'>
+                <h2 className='text-xs text-[#1271ff]'>12 oct 2024</h2>
+                
+            </div>
+        </div>
+        </div>
+        
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        
+      </CardFooter>
+    </Card>
+</div>
+<div>
+<Card className="">
+      <CardHeader>
+        <CardTitle>In Review</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+       
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        
+      </CardFooter>
+    </Card>
+</div>
+<div>
+<Card className="">
+      <CardHeader>
+        <CardTitle>Done</CardTitle>
+        <CardDescription>Deploy your new project in one-click.</CardDescription>
+      </CardHeader>
+      <CardContent>
+        
+      </CardContent>
+      <CardFooter className="flex justify-between">
+        
+      </CardFooter>
+    </Card>
+</div>
+    
       </div>
    </div>
     </section>
