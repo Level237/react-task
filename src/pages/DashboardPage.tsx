30da4aa81e6fe1b27d40e964ccc51867db1d92fc
level237 <Bramslevel129@gmail.com>
2024-10-01 10:10:28 +0100

    avatar update

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -33,7 +33,7 @@ export default function DashboardPage() {
          </div>
     
     
-      <div className="grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-3 gap-4">
          <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
             <p className="text-2xl text-gray-400 dark:text-gray-500">
                <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
