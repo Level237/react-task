ee8fcc8a6dfc32821cbab34fb577a93845bc61c4
level237 <Bramslevel129@gmail.com>
2024-10-01 19:06:34 +0100

    useForm in action

--- src/components/Task/AddTaskForm.tsx
+++ src/components/Task/AddTaskForm.tsx
@@ -40,24 +40,34 @@ export default function AddTaskForm() {
     valueIsValid:descriptionIsvalid
   }=useForm((value:any)=>value.trim()!=="")
 
+  const {
+    value:enterPriority,
+    hasError:priorityError,
+    valueHandler:enteredPriorityHandler,
+    blurHandler:blurPriorityHandler,
+    reset:resetPriority,
+    valueIsValid:priorityIsvalid
+  }=useForm((value:any)=>value.trim()!=="")
 
   const submitFormHandler=(event:any)=>{
     event.preventDefault()
-    if(!titleIsvalid && !descriptionIsvalid){
+    if(!titleIsvalid && !descriptionIsvalid  && !priorityIsvalid){
       return;
     }
     resetTitle()
     resetDescription()
+    resetPriority()
   }
   
   let formIsValid=false;
 
-  if(titleIsvalid && descriptionIsvalid){
+  if(titleIsvalid && descriptionIsvalid && priorityIsvalid){
     formIsValid=true;
   }
 
   const validClassTitle=titleError ? "border border-red-400" : ""
   const validClassDescription=descriptionError ? "border border-red-400" : ""
+  const validClassPriority=priorityError ? "border border-red-400" : ""
   return (
     <>
       <DialogContent className="sm:max-w-[480px]">
@@ -99,18 +109,18 @@ export default function AddTaskForm() {
                     <Label htmlFor="username" className="">
               Priority
             </Label>
-            <Select>
-      <SelectTrigger className="w-full">
-        <SelectValue placeholder=" Select a priority" />
-      </SelectTrigger>
-      <SelectContent>
-        <SelectGroup>
-          <SelectLabel>Priority</SelectLabel>
-          <SelectItem value="urgent">Urgent</SelectItem>
-          <SelectItem value="not urgent">Not Urgent</SelectItem>
-        </SelectGroup>
-      </SelectContent>
-    </Select>
+            <select 
+            onBlur={blurPriorityHandler} 
+            onChange={enteredPriorityHandler} 
+            value={enterPriority}
+            className={`${validClassPriority} bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}>
+    <option selected>Choose a priority</option>
+    <option value="0">urgent</option>
+    <option value="1">Not urgent</option>
+
+   
+          </select>
+          {priorityError && <p className="text-xs mt-[-10px] text-red-600">Ne peut pas etre vide</p>}
                     </div>
                     <div className="flex-1 flex flex-col gap-3">
                     <Label htmlFor="name" className="">
