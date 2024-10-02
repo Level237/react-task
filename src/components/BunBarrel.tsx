44adf2cc8214f91c1f21b96fd29ea1d429994124
level237 <Bramslevel129@gmail.com>
2024-10-02 23:19:39 +0100

    sync local to online

--- src/components/BunBarrel.tsx
+++ src/components/BunBarrel.tsx
@@ -35,11 +35,11 @@ export const BunBarrel:React.FC<{setCards:(tasks:TaskType[])=>void}>=({
     className={`mt-4 mb-4 h-44 shrink-0
     flex justify-center items-center rounded border text-3xl ${
       active
-      ? "border-red-800 bg-red-800/20 text-red-500"
-      : "border-red-500 bg-[#f9fafb] text-red-600"
+      ? "border-red-800 bg-red-800 text-white"
+      : "border-red-500/20 text-xl bg-[#000000ce] text-white"
     }
     `}>
-      {active ? <Trash/> : <Trash className="animate-bounce" />}
+      {active ? <Trash className='w-8 h-8'/> : <Trash className="animate-bounce w-8 h-8" />}
       
      
     </Card></>
