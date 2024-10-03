e29511ce4101799610e0f0b417f7988433b6cc2a
level237 <Bramslevel129@gmail.com>
2024-10-03 10:27:25 +0100

    update

--- src/components/BunBarrel.tsx
+++ src/components/BunBarrel.tsx
@@ -33,14 +33,14 @@ export const BunBarrel:React.FC<{setCards:(tasks:TaskType[])=>void}>=({
     onDragLeave={handleDragLeave}
     onDragOver={handleDragOver}
     className={`mt-4 mb-4 h-44 shrink-0
-    flex justify-center items-center rounded border text-3xl ${
+    flex justify-center items-center rounded border text-3xl hover:bg-red-400 cursor-pointer ${
       active
       ? "border-red-800 bg-red-800 text-white"
       : "border-red-500/20 text-xl bg-[#000000ad] text-white"
     }
     `}>
       {active ? <Trash className='w-8 h-8'/> : <Trash className="animate-bounce w-8 h-8" />}
-      
+      <h2 className='text-sm'>Corbeille</h2>
      
     </Card></>
    
